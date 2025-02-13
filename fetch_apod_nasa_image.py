330bc8afb7c1eac9724eb6374a7c85b9a219b602
GenmaHimmuro <t.suleimanov2@gmail.com>
2025-02-13 12:53:12 +0500

    Update fetch_apod_nasa_image.py

--- fetch_apod_nasa_image.py
+++ fetch_apod_nasa_image.py
@@ -2,17 +2,21 @@ import requests
 import os
 
 
-def get_apod_nasa_image(api_key_nasa):
-    payload = {'api_key': api_key_nasa, 'count': 30}
+def save_image(file_path, image_data):
+    with open(file_path, 'wb') as file:
+        file.write(image_data)
+
+
+def get_apod_nasa_image(api_key_nasa, image_count_apod):
+    payload = {'api_key': api_key_nasa, 'count': image_count_apod}
     url = 'https://api.nasa.gov/planetary/apod'
     response = requests.get(url, params=payload)
     response.raise_for_status()
     for ind, picture in enumerate(response.json()):
-        if picture['url'].endswith(('jpg', 'jpeg', 'png')):
-            filename = 'nasa_apod_{}.jpeg'.format(ind)
-            file_path = os.path.join('images', filename)
-            response = requests.get(picture['url'])
-            with open(file_path, 'wb') as file:
-                file.write(response.content)
-        else:
+        if not picture['url'].endswith(('jpg', 'jpeg', 'png')):
             continue
+        filename = 'nasa_apod_{}.jpeg'.format(ind)
+        file_path = os.path.join('images', filename)
+        response = requests.get(picture['url'])
+        response.raise_for_status()
+        save_image(file_path, image_data=response.content)
