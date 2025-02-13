ddd93f3da2a5affbf25038e4e346770b6c607cc2
GenmaHimmuro <t.suleimanov2@gmail.com>
2025-02-13 12:53:16 +0500

    Update fetch_epic_nasa_image.py

--- fetch_epic_nasa_image.py
+++ fetch_epic_nasa_image.py
@@ -3,32 +3,38 @@ import os
 import datetime
 
 
-def get_epic_nasa_image(api_key_nasa):
-    payload = {'api_key': api_key_nasa, 'count': 1}
+def save_image(file_path, image_data):
+    with open(file_path, 'wb') as file:
+        file.write(image_data)
+
+
+def get_epic_nasa_image(api_key_nasa, image_count_epic):
+    payload = {'api_key': api_key_nasa, 'count': image_count_epic}
     url_for_dates_and_pict = 'https://api.nasa.gov/EPIC/api/natural'
-    response_get_dates_and_pict = requests.get(url_for_dates_and_pict,
-                                               params=payload)
-    response_get_dates_and_pict.raise_for_status()
-    for ind, picture in enumerate(response_get_dates_and_pict.json()[:5]):
+    output_dates_and_images = requests.get(url_for_dates_and_pict,
+                                           params=payload)
+    output_dates_and_images.raise_for_status()
+
+    for ind, picture in enumerate(output_dates_and_images.json()[:5]):
         filename = 'nasa_EPIC_{}.png'.format(ind)
         file_path = os.path.join('images', filename)
 
         payload_download_pict = {
-                        'api_key': 'O4QVU4aPSsUjzqDLfH6z1pfbLkI3hysRHvXvAAng'
+                        'api_key': api_key_nasa
                                 }
-        url_download_pict = \
-            'https://api.nasa.gov/EPIC/archive/natural/'\
-            '{date}/{format}/{image}.png'.format(
-                                                date=datetime.date.fromisoformat(
-                                                     response_get_dates_and_pict.json()
-                                                     [ind]['date'][:10]).strftime('%Y/%m/%d'),
-                                                format='png',
-                                                image=response_get_dates_and_pict.json()
-                                                [ind]['image']
-                                                )
-        response_download_pict = requests.get(url_download_pict,
+        url_download_pict = 'https://api.nasa.gov/EPIC/archive/natural/'\
+                            '{date}/{format}/{image}.png'
+
+        output_date_of_image = output_dates_and_images.json()[ind]['date'][:10]
+        image_date = datetime.date.fromisoformat(
+                     output_date_of_image).strftime('%Y/%m/%d')
+        take_image = output_dates_and_images.json()[ind]['image']
+
+        substitute_route_paramaters = url_download_pict.format(date=image_date,
+                                                               format='png',
+                                                               image=take_image)
+
+        response_download_pict = requests.get(substitute_route_paramaters,
                                               params=payload_download_pict)
         response_download_pict.raise_for_status()
-
-        with open(file_path, 'wb') as file:
-            file.write(response_download_pict.content)
+        save_image(file_path, image_data=response_download_pict.content)
