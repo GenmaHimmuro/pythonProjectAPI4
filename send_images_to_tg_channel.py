1c07a51c2a92490c36bff7f6423c14a49942ef2b
GenmaHimmuro <t.suleimanov2@gmail.com>
2025-02-14 16:35:16 +0500

    Create send_images_to_tg_channel.py

---
+++ send_images_to_tg_channel.py
@@ -0,0 +1,14 @@
+import os
+import random
+import time
+
+
+def send_images_to_tg_channel(directory, bot, chat_id, time_interval_for_send):
+    files = os.listdir(directory)
+    random.shuffle(files)
+    while files:
+        file_name = files.pop()
+        file_path = os.path.join(directory, file_name)
+        with open(file_path, 'rb') as photo:
+            bot.send_photo(chat_id=chat_id, photo=photo)
+        time.sleep(time_interval_for_send)
