6af225c8dace6b0fd24d7708ac061308799e1f72
GenmaHimmuro <t.suleimanov2@gmail.com>
2025-02-17 12:10:04 +0500

    Update send_images_to_tg_channel.py

--- send_images_to_tg_channel.py
+++ send_images_to_tg_channel.py
@@ -2,6 +2,8 @@ import os
 import random
 import time
 
+from download_tools.download_image import send_image
+
 
 def send_images_to_tg_channel(directory, bot, chat_id, time_interval_for_send):
     files = os.listdir(directory)
@@ -9,6 +11,5 @@ def send_images_to_tg_channel(directory, bot, chat_id, time_interval_for_send):
     while files:
         file_name = files.pop()
         file_path = os.path.join(directory, file_name)
-        with open(file_path, 'rb') as photo:
-            bot.send_photo(chat_id=chat_id, photo=photo)
+        send_image(file_path, chat_id, bot)
         time.sleep(time_interval_for_send)
